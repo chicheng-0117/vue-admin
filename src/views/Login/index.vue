0d6520a283dc70e5832918ffd214d7ba4738ef56
chicheng-0117 <932185944@qq.com>
2021-01-16 17:31:06 +0800

    初始化项目

---
+++ src/views/Login/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="login">
+    登录页
+  </div>
+</template>
+<script>
+
+</script>
+<style lang="scss" scoped>
+  #login {
+    height: 100vh;
+    background-color: aqua;
+  }
+</style>
